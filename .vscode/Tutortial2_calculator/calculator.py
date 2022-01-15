079f6cd405fa75fe996681403b23f718cc3ba55b
thecodeguild <thecodeguild>
2022-01-15 10:54:17 +0530

    calculator added
    Signed-off-by: thecodeguild <thecodeguild>

---
+++ .vscode/Tutortial2_calculator/calculator.py
@@ -0,0 +1,24 @@
+# We will be learning few conversation of datatypes
+
+# 1) str to int : any string that is has a number or integer can be converted to int or float type 
+#     int("45") OR int("45.5")
+# 2) int to str : any number can be converted to a string 
+#     str(54) 
+
+
+a= "45"
+b= ""
+print(type(int(a)))
+
+# We will learn about breaking a string
+
+command = input("Enter your equation :")
+print(command)
+
+# you will be able to see the equation as is but here we want to convert the equation into math 
+# for doing the same we will have to break down the string into parts and then convert 
+
+# We will also learn about list
+
+l=[ 1, 2, 3]
+print(sum(l))
