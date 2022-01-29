589bc6bc568ae2df9edc15c9dcaaecd792f826e4
thecodeguild <thecodeguild>
2022-01-29 12:46:25 +0530

    calculator
    Signed-off-by: thecodeguild <thecodeguild>

--- Tutortial2_calculator/calculator.py
+++ Tutortial2_calculator/calculator.py
@@ -1,24 +1,5 @@
-# We will be learning few conversation of datatypes
+# use a variable to take the input equation
+equation = input("Please enter the equation: ")
 
-# 1) str to int : any string that is has a number or integer can be converted to int or float type 
-#     int("45") OR int("45.5")
-# 2) int to str : any number can be converted to a string 
-#     str(54) 
-
-
-a= "45"
-b= ""
-print(type(int(a)))
-
-# We will learn about breaking a string
-
-command = input("Enter your equation :")
-print(command)
-
-# you will be able to see the equation as is but here we want to convert the equation into math 
-# for doing the same we will have to break down the string into parts and then convert 
-
-# We will also learn about list
-
-l=[ 1, 2, 3]
-print(sum(l))
+# Let's check the type of the variable, this is to check if the variable can be used for calculation  
+print("type of the variable equation is ", type(equation))
